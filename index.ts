36fff31b73b99c863df65413cc77afdf03783499
Fernando Isidro L <luffynando@gmail.com>
2025-05-11 00:18:01 -0600

    feat: :sparkles: add exports for filters, includes and sorts by library

--- index.ts
+++ index.ts
@@ -17,3 +17,18 @@ export { InvalidSortQuery } from './src/exceptions/invalid_sort_query.js';
 export { UnknownIncludedFieldsQuery } from './src/exceptions/unknown_included_fields_query.js';
 export { stubsRoot } from './stubs/main.js';
 export * from 'collect.js';
+// Filters
+export { FiltersBeginWithStrict } from './src/filters/filters_begin_with_strict.js';
+export { FiltersCallback } from './src/filters/filters_callback.js';
+export { FiltersEndsWithStrict } from './src/filters/filters_ends_with_strict.js';
+export { FiltersExact } from './src/filters/filters_exact.js';
+export { FiltersOperator } from './src/filters/filters_operator.js';
+export { FiltersPartial } from './src/filters/filters_partial.js';
+export { FiltersTrashed } from './src/filters/filters_trashed.js';
+// Includes
+export { IncludedCallback } from './src/includes/included_callback.js';
+export { IncludedCount } from './src/includes/included_count.js';
+export { IncludedRelationship } from './src/includes/included_relationship.js';
+// Sorts
+export { SortsCallback } from './src/sorts/sorts_callback.js';
+export { SortsField } from './src/sorts/sorts_field.js';
